571f6699725d5cbce6c839855dad14d6ae258f14
Sam Scolari <56174573+Sam-Scolari@users.noreply.github.com>
2023-06-15 16:14:45 -0500

    Create index.test.ts

---
+++ index.test.ts
@@ -0,0 +1,19 @@
+import { sepolia } from "viem/chains";
+import { contracts } from "./botswarm.config";
+import BotSwarm from "./src/BotSwarm";
+import castVote from "./src/tasks/castVote";
+
+const { NounsPool } = contracts.sepolia;
+
+const { watch, log, addTask } = BotSwarm();
+
+watch(NounsPool, "BidPlaced", ({ blockNumber, args }) => {
+  log.info("Bid placed on Nouns Pool");
+
+  addTask({
+    id: `castVote:${args.propId}`,
+    chain: sepolia,
+    block: blockNumber,
+    task: castVote,
+  });
+});
