a332f208d6e655db75e493575fd13087a6801550
Sam Scolari <56174573+Sam-Scolari@users.noreply.github.com>
2023-06-28 23:18:22 -0500

    Update botswarm.config.ts

--- botswarm.config.ts
+++ botswarm.config.ts
@@ -7,7 +7,10 @@ export default createConfig({
   NounsPool: {
     abi: NounsPool,
     deployments: [
-      { chain: mainnet, address: "0xFBA3912Ca04dd458c843e2EE08967fC04f3579c2" },
+      {
+        chain: mainnet,
+        address: "0xBE5E6De0d0Ac82b087bAaA1d53F145a52EfE1642",
+      },
       { chain: sepolia, address: "0xd27dfb807DC3435AC3e14b55FcF1B50F96fF769a" },
     ],
   },
